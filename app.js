d0bb5affd8e9f154338ff93c2281402a5c627205
DorShireto <shireto.dor@gmail.com>
2021-04-27 10:25:55 +0300

    new menu, modal fixed

--- app.js
+++ app.js
@@ -748,6 +748,11 @@ $(function () {
 		changePage("loginPage");
 	});
 });
+$(function () { //for login btn at rgister page
+	$("#login1").click(function (e) {
+		changePage("loginPage");
+	});
+});
 
 $(function () {
 	$("#loginForm").submit(function (e) {
@@ -782,17 +787,36 @@ $(function () {
 
 
 // ***************  About ******************//
+
+function closeModal(modal){
+	// document.getElementById("about").hidden = false;
+	modal.style.display = "block";
+}
+
 $(function () {//modal
 	// Get the modal
 	var modal = document.getElementById("aboutModal");
 	// Get the button that opens the modal
 	var btn = document.getElementById("aboutBtn");
+
+	var btnS = document.getElementsByClassName("aboutBtnClass")
+
+	for(let i = 0; i < btnS.length; i++){
+		// console.log(btnS.length);
+		btnS[i].onclick = function () {
+			modal.style.display = "block";
+		}
+	}
+
 	// Get the <span> element that closes the modal
 	var span = document.getElementsByClassName("close")[0];
 	// When the user clicks the button, open the modal 
-	btn.onclick = function () {
-		modal.style.display = "block";
-	}
+	// btn.onclick = function () {
+	// 	modal.style.display = "block";
+	// }
+
+	// btn.onclick = closeModal(modal)
+	// btn1.onclick = closeModal(modal)
 
 	// When the user clicks on <span> (x), close the modal
 	span.onclick = function () {
@@ -805,6 +829,14 @@ $(function () {//modal
 			modal.style.display = "none";
 		}
 	}
+
+
+
+
+
+
+
+
 	// When the user clicks esc, close it
 	window.addEventListener('keydown', function (event) {
 		if (event.key === 'Escape') {
@@ -812,6 +844,14 @@ $(function () {//modal
 		}
 	})
 });
+
+// Help function to close modal when clicking on the footer area
+function closeModalFooterClick(){
+	let modal = document.getElementById("aboutModal");
+	if(modal.style.display != 'none'){
+		modal.style.display = 'none';
+	}
+}
 // ***************  About END ******************//
 
 
